c494dfc24597137a907211bcba250a899a303828
Jay <jaykang445@gmail.com>
2025-01-05 21:01:18 +0000

    update to beta-2 gradlerio

--- src/main/java/frc/robot/subsystems/swerve/Drive.java
+++ src/main/java/frc/robot/subsystems/swerve/Drive.java
@@ -88,8 +88,6 @@ public class Drive extends SubsystemBase {
     /* use kinematics to get desired module states */
     ChassisSpeeds discretizedSpeeds =
         ChassisSpeeds.discretize(targetSpeeds, Constants.PERIODIC_LOOP_SEC);
-    /* ChassisSpeeds discretizedSpeeds = targetSpeeds; // FIXME
-    discretizedSpeeds.discretize(Constants.PERIODIC_LOOP_SEC); */
 
     SwerveModuleState[] moduleTargetStates = KINEMATICS.toSwerveModuleStates(discretizedSpeeds);
     SwerveDriveKinematics.desaturateWheelSpeeds(
